ce429aced8cb9b8be4162d7b4f052627a2f8464c
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-19 19:34:36 -0500

    said hello in Java with name input

---
+++ datatypes/datatypes-exercises/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, what is your name:");
+        String name = input.nextLine();
+        System.out.println(String.format("Hello, %s!", name));
+    }
+
+}
