611bac343314100b0bb93f3fa7bf5c6249dff05e
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-19 19:02:44 -0500

    Added language with examples

--- datatypes/datatype-examples/src/main/java/org/launchcode/Message.java
+++ datatypes/datatype-examples/src/main/java/org/launchcode/Message.java
@@ -8,6 +8,8 @@ public class Message {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
+        } else if (lang.equals("tag")) {
+            return "Kamusta, sandaigdigan!";
         } else {
             return "Hello, World!";
         }
