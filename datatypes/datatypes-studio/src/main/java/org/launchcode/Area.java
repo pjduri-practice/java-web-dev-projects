644fa85a427c2c29a7f0503f7faa98e28158ddf8
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-19 23:32:20 -0500

    datatypes studio part b - Circle class with getArea method

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -9,7 +9,7 @@ public class Area {
         Scanner input = new Scanner(System.in);
         System.out.println("What is the radius of your circle?");
         double radius = input.nextDouble();
-        double area = pi * radius * radius;
+        double area = Circle.getArea(radius);
         System.out.println("The area of your circle is: " + area);
     }
 
