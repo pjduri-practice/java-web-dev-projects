731cfe2940690598b42037603698bb4375266b3e
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-19 23:20:56 -0500

    datatypes studio part A - area class has main method which calculates area of circle based on user input radius

---
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -0,0 +1,16 @@
+package org.launchcode;
+import java.util.Scanner;
+
+public class Area {
+
+    public static void main(String[] args) {
+
+        double pi = 3.14;
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the radius of your circle?");
+        double radius = input.nextDouble();
+        double area = pi * radius * radius;
+        System.out.println("The area of your circle is: " + area);
+    }
+
+}
