0c31830faebb94c2a8c9dbcdbc4e48ceb8d41643
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-28 02:27:39 -0500

    classes 1 exercises Student class had constructor, getters/setters and an instance in StudentPractice

--- classes-part-one/exercises/src/main/java/org/launchcode/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/org/launchcode/StudentPractice.java
@@ -1,5 +1,9 @@
+package org.launchcode;
+
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student student = new Student("Pat", 1, 1, 4.0);
+        System.out.println(student.getName());
     }
 }
