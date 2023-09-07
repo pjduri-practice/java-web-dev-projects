c07b4a4b3bbead3b12550554fb569dacecec966a
Your Name <108307350+pjduri@users.noreply.github.com>
2023-09-07 16:45:55 -0500

    Classes part 2 exercises done

--- classes-part-2/exercises/src/main/java/org/launchcode/Student.java
+++ classes-part-2/exercises/src/main/java/org/launchcode/Student.java
@@ -1,5 +1,7 @@
 package org.launchcode;
 
+import java.util.Objects;
+
 public class Student {
 
     private static int nextStudentId = 1;
@@ -25,25 +27,58 @@ public class Student {
     }
 
     public String studentInfo() {
-        return (this.name + " has a GPA of: " + this.gpa);
+//        return (this.name + " has a GPA of: " + this.gpa);
+        return "ID: " + getStudentId() + "\n" +
+                "Name: " + getName() + "\n" +
+                "Credits: " + getNumberOfCredits() + "\n" +
+                "GPA: " + gpa;
     }
 
 
     //TODO: Uncomment and complete the getGradeLevel method here:
-//    public String getGradeLevel() {
-//        // Determine the grade level of the student based on numberOfCredits
-//    }
+    public String getGradeLevel() {
+        // Determine the grade level of the student based on numberOfCredits
+        return numberOfCredits >= 90 ? "Senior"
+                : numberOfCredits >= 60 ? "Junior"
+                : numberOfCredits >= 30 ? "Sophomore"
+                : "Freshman";
+    }
 
     // TODO: Complete the addGrade method.
     public void addGrade(int courseCredits, double grade) {
         // Update the appropriate fields: numberOfCredits, gpa
+        double totalQualityScore = gpa * numberOfCredits;
+        totalQualityScore += grade * courseCredits;
+        numberOfCredits += courseCredits;
+        gpa = totalQualityScore / numberOfCredits;
     }
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
     //  than just the class fields.
+    @Override
+    public String toString() {
+        return "Name: " + getName() + "\n" +
+                "Credits: " + getNumberOfCredits() + "\n" +
+                "GPA: " + gpa;
+    }
+
 
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Student objects equal.
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return studentId == student.studentId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId);
+    }
 
     public String getName() {
         return name;
@@ -79,11 +114,11 @@ public class Student {
 
     public static void main(String[] args) {
         Student sally = new Student("Sally",1,1,4.0);
-        System.out.println("The Student class works! " + sally.getName() + " is a student!");
-        System.out.println(sally);
+//        System.out.println("The Student class works! " + sally.getName() + " is a student!");
+        System.out.println(sally.studentInfo());
         sally.addGrade(12, 3.5);
-        System.out.println(sally);
+        System.out.println(sally.studentInfo());
         sally.addGrade(25, 3.8);
-        System.out.println(sally);
+        System.out.println(sally.studentInfo());
     }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,19 @@
 package org.launchcode;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        MenuItem item = new MenuItem(2.66, "Tasty Toasted Ravs", "Appetizers", true);
+        ArrayList<MenuItem> items = new ArrayList<>();
+        items.add(item);
+        Date now = new Date();
+
+        Menu menu = new Menu(now, items);
+        System.out.println(menu.getLastUpdated());
     }
 }
