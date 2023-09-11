c0becc6c54bf4d1b3a8da07a04f6ebba1c28cfce
Your Name <108307350+pjduri@users.noreply.github.com>
2023-09-11 18:01:41 -0500

    ch6 studio started

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -7,7 +7,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class BalancedBracketsTest {
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void printExampleStrings() {
+        System.out.println("Expect false: " + BalancedBrackets.hasBalancedBrackets("sdg[[bd[bbd]x]]xb]b]b]b]]]"));
+        System.out.println("Expect true: " + BalancedBrackets.hasBalancedBrackets("sdg[[bd[bb[[[[[[d]x]]xb]b]b]b]]]"));
+        System.out.println("Expect true: " + BalancedBrackets.hasBalancedBrackets("]["));
+        System.out.println("Expect true: " + BalancedBrackets.hasBalancedBrackets("]]]afd[[sdfd]asfd[[[asdf]]sfd["));
     }
 }
