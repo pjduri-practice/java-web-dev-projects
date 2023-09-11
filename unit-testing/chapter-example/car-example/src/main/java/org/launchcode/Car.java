822ef08c3d3af88a2fb73ac7609b4ab1352088c5
Your Name <108307350+pjduri@users.noreply.github.com>
2023-09-11 17:22:52 -0500

    ch 6 unit testing exercises

--- unit-testing/chapter-example/car-example/src/main/java/org/launchcode/Car.java
+++ unit-testing/chapter-example/car-example/src/main/java/org/launchcode/Car.java
@@ -84,4 +84,8 @@ public class Car {
         this.odometer += milesAbleToTravel;
     }
 
+    public void addGas(double gas) {
+        this.setGasTankLevel(gas + this.getGasTankLevel());
+    }
+
 }
